dc049a5628555c4563769e998165c54b0f463f79
Usama <usamariaz@aptechnorth.edu.pk>
2026-08-18 12:55:29 +0500

    till Add and view product API implementation on frontend

--- ReactWithBackend/src/Component/ProductsComponent/AddProduct.jsx
+++ ReactWithBackend/src/Component/ProductsComponent/AddProduct.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 export const AddProduct = () => {
+
+
+
+  const [isProductData, setProductData] = useState({});
+
+
+
+  const createProduct = async (e) => {
+    e.preventDefault();
+
+    console.log(isProductData)
+
+  try {
+    const response = await axios.post(
+      "http://localhost:3000/api/v1/addProduct",
+      {
+       prodName: isProductData.prodName,
+       prodPrice: Number(isProductData.prodPrice),
+       prodDesc: isProductData.prodDesc,
+       prodRating: Number(isProductData.prodRating),
+       prodImage: isProductData.prodImage,
+
+      }
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log("Request completed");
+  }
+};
+
+
   return (
-    <div>AddProduct</div>
+    <div>
+
+      <h1  className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight text-center">Add Product</h1>
+
+      <form className="max-w-sm mx-auto">
+        <div className="mb-5">
+          <label className="block mb-2.5 text-sm font-medium text-heading">Product Name</label>
+          <input type="text"  className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" onChange={(e)=>setProductData({...isProductData, prodName: e.target.value})} required />
+        </div>
+
+        <div className="mb-5">
+          <label className="block mb-2.5 text-sm font-medium text-heading">Product Price</label>
+          <input type="text" className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"  onChange={(e)=>setProductData({...isProductData, prodPrice: e.target.value})} required />
+        </div>
+        <div className="mb-5">
+          <label className="block mb-2.5 text-sm font-medium text-heading">Product Rating</label>
+          <input type="text" className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"  onChange={(e)=>setProductData({...isProductData, prodRating: e.target.value})} required />
+        </div>
+        <div className="mb-5">
+          <label className="block mb-2.5 text-sm font-medium text-heading">Product Description</label>
+          <textarea type="text" className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"  onChange={(e)=>setProductData({...isProductData, prodDesc: e.target.value})} required />
+        </div>
+        <div className="mb-5">
+          <label className="block mb-2.5 text-sm font-medium text-heading">Product Image</label>
+          <input type="text" className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"  onChange={(e)=>setProductData({...isProductData, prodImage: e.target.value})} required />
+        </div>
+       
+        <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={createProduct}>Add Product</button>
+      </form>
+
+    </div>
+
   )
 }
--- ReactWithBackend/src/Component/ProductsComponent/ViewProduct.jsx
+++ ReactWithBackend/src/Component/ProductsComponent/ViewProduct.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export const ViewProduct = () => {
+
+
+  const [isProducts, setProducts] = useState([])
+
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/viewProduct",
+      );
+      console.log(response.data);
+
+      setProducts(response.data)
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log("Request completed");
+    }
+  };
+
+
+  useEffect(() => {
+    getProducts();
+  }, [])
+
+
+
   return (
-    <div>ViewProduct</div>
+    <div>
+      {isProducts.map(({_id,prodName, prodPrice, prodDesc, prodImage, prodRating},i) => (
+
+        <div key={i} className="bg-neutral-primary-soft block max-w-sm border border-default rounded-base shadow-xs">
+          <a href="#">
+            <img className="rounded-t-base" src={prodImage} alt="" />
+          </a>
+          <div className="p-6 text-center">
+            <span className="inline-flex items-center bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium px-1.5 py-0.5 rounded-sm">
+
+              {prodRating}
+            </span>
+            <a href="#">
+              <h5 className="mt-3 mb-6 text-2xl font-semibold tracking-tight text-heading">{prodName}</h5>
+            </a>
+          <p className="mb-6 text-body">{prodDesc}</p>
+          <p className="mb-6 text-body">{prodPrice}</p>
+          </div>
+        </div>
+
+      ))}
+
+
+
+    </div>
   )
 }
